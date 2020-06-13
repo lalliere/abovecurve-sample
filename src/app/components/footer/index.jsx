cd76068f5826c57cfb94f670d5cf07a30ec5fa07
lalliere <emilylallier@yahoo.com>
2020-06-13 12:55:15 -0400

    Added Footer Component

---
+++ src/app/components/footer/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./assets/ac_logo_24.png";
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "10vh",
+    position: "relative",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    zIndex: "2",
+    boxShadow: "0 2px 12px 6px rgba(0, 0, 0, 0.1)",
+  }
+}))
+
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+    <BottomNavigation className={classes.root} showLabels>
+      <img src={logo} alt="Above Curve" data-testid="ac-footer-logo"></img>
+    </BottomNavigation>
+  );
+}
