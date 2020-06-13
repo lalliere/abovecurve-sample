01d9cd3da37c684f9828400f202487223c6fccbe
Danielle Daley <danielledaley2196@gmail.com>
2020-06-13 13:27:52 -0400

    added server things

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+// api.abovecurve.com/
+//router.use("/health", require("./health"));
+//router.use("/ping", require("./ping"));
+router.use("/users", require("./users"));
+
+router.get("/", function (req, res, next) {
+  res.json({
+    welcome:
+      "Welcome to the Above Curve API. Here you will find data-sets on various fields of research.",
+    dataSets: ["health"],
+    links: [{ health: `/health` }],
+    help:
+      "This is the API, if you are looking for the visual data tools please see https://abovecurve.com",
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,46 @@
+const createError = require("http-errors");
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const passport = require("passport");
+
+const routes = require("./routes");
+const users = require("./routes/users");
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Public API Routes
+app.use("/", routes);
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/users", users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ message: "Server error" });
+});
+
+module.exports = app;
