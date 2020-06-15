33a9d8aa70214e0de6e9fc6f1fa1b855f309190a
lalliere <emilylallier@yahoo.com>
2020-06-15 17:20:45 -0400

    build folder

---
+++ build/precache-manifest.63d87f404aaeecd36683426d4e85a1dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0a2accac6ede556c56e284ffa01da6",
+    "url": "/abovecurve-sample/index.html"
+  },
+  {
+    "revision": "89cb0d712dd2d1cd8b86",
+    "url": "/abovecurve-sample/static/css/main.35b4be56.chunk.css"
+  },
+  {
+    "revision": "48449651f96859852ea0",
+    "url": "/abovecurve-sample/static/js/2.1d2e91c1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/abovecurve-sample/static/js/2.1d2e91c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89cb0d712dd2d1cd8b86",
+    "url": "/abovecurve-sample/static/js/main.150d15eb.chunk.js"
+  },
+  {
+    "revision": "3bdab56b9f7c2bacf6d1",
+    "url": "/abovecurve-sample/static/js/runtime-main.d26b50d2.js"
+  },
+  {
+    "revision": "92950c754d1f6c6e57d8385090fdddd2",
+    "url": "/abovecurve-sample/static/media/aboveCurve.92950c75.png"
+  },
+  {
+    "revision": "1cbab3467f76bcc32b4dbb80bdac48a8",
+    "url": "/abovecurve-sample/static/media/viewbyState.1cbab346.png"
+  },
+  {
+    "revision": "f6ba13cdd0b2a74ef0cc6e1c728788aa",
+    "url": "/abovecurve-sample/static/media/viewbyUS.f6ba13cd.png"
+  }
+]);
